bedd73ed1cab6be18e93d4dd83ce41bf4c22e385
Le Dinh <lenguyennhutdinh@gmail.com>
2024-04-14 10:48:30 +0700

    fix getUrlParams

--- app/video-call/page.tsx
+++ app/video-call/page.tsx
@@ -6,12 +6,16 @@ import { Box } from '@mui/material'
 
 export interface VideoCallProps {}
 
-export function getUrlParams(url = window.location.href) {
-  let urlStr = url.split('?')[1]
-  return new URLSearchParams(urlStr)
-}
+// function getUrlParams(url = window.location.href) {
+//   let urlStr = url.split('?')[1]
+//   return new URLSearchParams(urlStr)
+// }
 
 export default function VideoCall(props: VideoCallProps) {
+  function getUrlParams(url = window.location.href) {
+    let urlStr = url.split('?')[1]
+    return new URLSearchParams(urlStr)
+  }
   const roomID = getUrlParams().get('roomID') || randomID(5)
   const zegoInstanceRef = useRef<any>(null)
 

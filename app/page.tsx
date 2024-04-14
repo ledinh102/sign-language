f8c110ab72c113c938fe9afe3e471a8c2c3a48d7
Le Dinh <lenguyennhutdinh@gmail.com>
2024-04-14 11:12:38 +0700

    add isWebcamOn

--- app/page.tsx
+++ app/page.tsx
@@ -8,23 +8,8 @@ export default function Home() {
     <div>
       <OptionList isRevert={isRevert} />
       <LanguageList isRevert={isRevert} />
-      <Content isRevert={isRevert} />
+      <Content isRevert={isRevert} isWebcamOn={false} />
     </div>
   )
-  // return <pose-viewer></pose-viewer>
-  // const [text, setText] = useState('Hello')
-  // const [value] = useDebounce(text, 1000)
-
-  // return (
-  //   <div>
-  //     <input
-  //       defaultValue=''
-  //       onChange={e => {
-  //         setText(e.target.value)
-  //       }}
-  //     />
-  //     <p>Actual value: {text}</p>
-  //     <p>Debounce value: {value}</p>
-  //   </div>
   // )
 }

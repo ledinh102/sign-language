9b81783a3bd4690f04ef8a124efcc195799453f9
Le Dinh <lenguyennhutdinh@gmail.com>
2024-05-22 06:38:04 +0700

    fix deploy vercel

--- app/test1/page.tsx
+++ app/test1/page.tsx
@@ -3,18 +3,26 @@ import { useState } from 'react'
 
 function App() {
   const [ourText, setOurText] = useState('')
-  const msg = new SpeechSynthesisUtterance()
+  let msg = null
 
-  const speechHandler = (msg: SpeechSynthesisUtterance) => {
-    msg.text = ourText
-    window.speechSynthesis.speak(msg)
+  if (typeof window !== 'undefined' && typeof window.SpeechSynthesisUtterance !== 'undefined') {
+    msg = new SpeechSynthesisUtterance()
+  }
+
+  const speechHandler = () => {
+    if (msg) {
+      msg.text = ourText
+      window.speechSynthesis.speak(msg)
+    } else {
+      console.error('SpeechSynthesisUtterance is not supported in this environment.')
+    }
   }
 
   return (
     <div className='App'>
       <h1>React Text to Speech App</h1>
       <input type='text' value={ourText} placeholder='Enter Text' onChange={e => setOurText(e.target.value)} />
-      <button onClick={() => speechHandler(msg)}>SPEAK</button>
+      <button onClick={speechHandler}>SPEAK</button>
     </div>
   )
 }

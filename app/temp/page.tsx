bca62a9722f272d31dec7966504426a8fce166d4
Le Dinh <lenguyennhutdinh@gmail.com>
2024-05-25 16:30:06 +0700

    change api

--- app/temp/page.tsx
+++ app/temp/page.tsx
@@ -12,7 +12,7 @@ const UploadFile = () => {
     if (file) {
       const formData = new FormData()
       formData.append('file', file)
-
+      //
       const response = await fetch('https://172.25.41.23:8000/audio-to-text', {
         method: 'POST',
         body: formData

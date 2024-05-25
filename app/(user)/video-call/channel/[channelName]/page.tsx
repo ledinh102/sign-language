b802d91b5ae7d86c65ae8d6884b3533686624021
Le Dinh <lenguyennhutdinh@gmail.com>
2024-05-25 21:57:18 +0700

    remove login 1 in video call

--- app/(user)/video-call/channel/[channelName]/page.tsx
+++ app/(user)/video-call/channel/[channelName]/page.tsx
@@ -6,8 +6,8 @@ import { redirect } from 'next/navigation'
 
 export default function Page({ params }: { params: { channelName: string } }) {
   const { data: session, status } = useSession()
-  if (status === 'loading') return <LinearProgressCustom />
-  else if (status === 'unauthenticated') redirect(`/auth/sign-in?callbackUrl=/video-call/channel/${params.channelName}`)
+  // if (status === 'loading') return <LinearProgressCustom />
+  // else if (status === 'unauthenticated') redirect(`/auth/sign-in?callbackUrl=/video-call/channel/${params.channelName}`)
 
   return <Call appId={process.env.NEXT_PUBLIC_AGORA_APP_ID!} channelName={params.channelName} />
 }
